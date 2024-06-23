fdd54bfc84f2ac65216908c4c5ef8fdb852cea8f
Mehul Thuletiya <210303108237@paruluniversity.ac.in>
2024-06-23 23:27:28 +0530

    Update

--- src/Components/NoteContainer.js
+++ src/Components/NoteContainer.js
@@ -6,6 +6,7 @@ import SearchBar from "./SearchBar";
 function NoteContainer(props) {
   const reverseArray = (arr) => {
     const array = [];
+
     for (let i = arr.length - 1; i >= 0; i--) {
       array.push(arr[i]);
     }
@@ -13,26 +14,31 @@ function NoteContainer(props) {
   };
 
   const notes = reverseArray(props.notes);
+  console.log("Setted Note");
 
-  const [displayNotes, setDisplayNotes] = useState(notes);
-
+  const [displayNotes, setdisplayNotes] = useState(notes);
+  const [ver, setVer] = useState(false);
   const searchHandler = (event) => {
-    event.preventDefault();
-    let searchQuery = event.target.value.toLowerCase();
-    let filteredNotes = notes.filter((el) => {
-      let searchValue = el.text.toLowerCase();
-      return searchValue.indexOf(searchQuery) !== -1;
-    });
-    setDisplayNotes(filteredNotes);
-  };
+    event.preventDefault(); //Search Function
+    setVer(true);
+    console.log("You Searched");
 
+    let searchQuery = event.target.value.toLowerCase(),
+      filteredNotes = notes.filter((el) => {
+        let SearchValue = el.text.toLowerCase();
+        console.log(SearchValue.indexOf(searchQuery));
+        return SearchValue.indexOf(searchQuery) !== -1;
+      });
+
+    setdisplayNotes(filteredNotes);
+  };
   const deleteNote = (id) => {
-    let updatedNotes = notes.filter((note) => note.id !== id);
-    setDisplayNotes(updatedNotes);
+    setdisplayNotes(notes);
     props.deleteNote(id);
   };
-
   const updateText = (text, id) => {
+    setdisplayNotes(notes);
+
     props.updateText(text, id);
   };
 
@@ -42,16 +48,19 @@ function NoteContainer(props) {
         <SearchBar notes={notes} searchHandler={searchHandler} />
         <h2>My Notes</h2>
         <div className="note-container-notes custom-scroll">
-          {displayNotes.length > 0 ? (
-            displayNotes.map((note) => (
-              <div className="note-container-note" key={note.id}>
-                <Notes
-                  note={note}
-                  deleteNote={() => deleteNote(note.id)}
-                  updateText={() => updateText(note.text, note.id)}
-                />
-              </div>
-            ))
+          {console.log(displayNotes)}
+          {notes?.length > 0 ? (
+            notes.map((note) => {
+              return (
+                <div className="note-container-note" key={note.id}>
+                  <Notes
+                    note={note}
+                    deleteNote={() => deleteNote(note.id)}
+                    updateText={() => updateText(note.text, note.id)}
+                  />
+                </div>
+              );
+            })
           ) : (
             <div className="note-container-note">
               <h3>No Notes Present</h3>
